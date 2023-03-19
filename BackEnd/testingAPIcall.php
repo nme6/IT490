18c33e05cb4956282e03b797f27581857a7588df
Ellis <eab4@njit.edu>
2023-03-19 00:30:54 -0400

    intermediate code for receiving information from front-end container and preparing to populate/check the database

--- BackEnd/testingAPIcall.php
+++ BackEnd/testingAPIcall.php
@@ -1,9 +1,11 @@
 <?php
 require __DIR__ . '/testingAPI.php';
+#update this in the future to check for other conditions the user might want to see, like evolution lines, typing, movesets, etc
 $poke_int = (int)readline('Enter the pokemon number: ');
 $get_data = callAPI('GET', 'https://pokeapi.co/api/v2/pokemon/' . $poke_int, false);
 $response = json_decode($get_data, true);
-echo $response['name'] . "\n";
+
+echo ucfirst($response['name'] . "\n");
 #$errors = $response['response']['errors'];
 #$data = $response['response']['data'][0];
 ?>
