22aeccd8f94fb813436f5144fbc0bd064743da5e
Ellis <eab4@njit.edu>
2023-03-19 18:39:12 -0400

    successful registration with database

---
+++ BackEnd/stepTest.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+// Create a connection to RabbitMQ
+$connection = new AMQPStreamConnection('192.168.191.111', 5672, 'admin', 'admin');
+$channel = $connection->channel();
+
+// Declare the queue
+$channel->queue_declare('DB2BE', false, false, false, false);
+
+echo "-={[BackEnd] Waiting for Database confirmation. To exit press CTRL+C}=-\n";
+
+// Define the callback function to process messages from the queue
+$callback = function ($message) use ($channel) {
+
+    $data = $message->body;
+
+   // $userExists = $data['userExists'];    
+    echo "Received user status: " . $message->body . "\n";
+
+
+
+    $userStatusConnection = new AMQPStreamConnection('192.168.191.111', 5672, 'admin', 'admin');
+    $userStatusChannel = $userStatusConnection->channel();
+    $userStatusChannel->queue_declare('BE2FE', false, false, false, false);
+    $userStatusMessage = new AMQPMessage($data);
+    $userStatusChannel->basic_publish($userStatusMessage, '', 'BE2FE');
+    $userStatusChannel->close();
+    $userStatusConnection->close();
+};
+
+// Consume messages from the queue
+$channel->basic_consume('DB2BE', '', false, true, false, false, $callback);
+
+// Keep consuming messages until the channel is closed
+while ($channel->is_open()) {
+    $channel->wait();
+}
+
+// Close the connection
+$channel->close();
+$connection->close();
