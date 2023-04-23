fd8ab92df32740d83c8ccc8f556f04435432bcfe
Ellis <eab4@njit.edu>
2023-04-23 14:01:46 -0400

    first step in locally storing API call response information

--- BackEnd/testingAPIcall.php
+++ BackEnd/testingAPIcall.php
@@ -1,11 +1,34 @@
 <?php
-require __DIR__ . '/testingAPI.php';
-#update this in the future to check for other conditions the user might want to see, like evolution lines, typing, movesets, etc
-$poke_int = (int)readline('Enter the pokemon number: ');
-$get_data = callAPI('GET', 'https://pokeapi.co/api/v2/pokemon/' . $poke_int, false);
-$response = json_decode($get_data, true);
 
-echo ucfirst($response['name'] . "\n");
+/*
+ *Possible implementation for a check to see if the number associated is already stored in the database: that way we can refrain from constant calls
+ *
+ */
+require __DIR__ . '/testingAPI.php';
+
+$poke_int = 0;
+
+while ($poke_int != -1) {
+	#update this in the future to check for other conditions the user might want to see, like evolution lines, typing, movesets, etc
+	$poke_int = (int)readline('Enter the pokemon number (-1 to exit): ');
+
+	if ($poke_int != -1) {
+		$get_data = callAPI('GET', 'https://pokeapi.co/api/v2/pokemon/' . $poke_int, false);
+		$response = json_decode($get_data, true);
+
+		$file = fopen('APIResponses.txt', 'a');
+
+		fwrite($file, ucfirst($poke_int . ": "));
+
+		fwrite($file, ucfirst($response['name'] . "\n"));
+
+		fclose($file);
+	
+
+		echo ucfirst($response['name'] . "\n");
+	}
+
+}
 #$errors = $response['response']['errors'];
 #$data = $response['response']['data'][0];
 ?>
