121ee92830b2c14867fb3b5da5c342e14e0b2ab0
Ellis <eab4@njit.edu>
2023-04-25 23:17:10 -0400

    Working api calls with typing, evolution chains, and damage relations

--- BackEnd/pokeAPI.php
+++ BackEnd/pokeAPI.php
@@ -2,48 +2,77 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PokePHP\PokeApi;
 $api = new PokeApi;
-$choice = readline('Please enter what you are looking for: ');
+$choice = null;
+while ($choice != 'exit') {
+	$choice = readline('Please enter what you are looking for: ');
+	//Check for when the user wants to check the evolution chains
+	//NOTE: As of 4/25/23 the number associated is the evolution chain grouping number
+	if ($choice == 'evolution chain') {
+		$user_input = (int)readline('Enter pokemon number: ');
 
-if ($choice == 'evolution chain') {
-	$user_input = (int)readline('Enter pokemon number: ');
+		$result = $api->evolutionChain($user_input);
+		$decoded_result = json_decode($result, true);
+		
+		//store the decoded api response array
+		$chain = $decoded_result['chain'];
 
-	$result = $api->evolutionChain($user_input);
-	$decoded_result = json_decode($result, true);
+		echo "Evolution Chain:\n";
+		
+		//while there are still items in the chain array continue the loop
+		while (!empty($chain)) {
+			echo $chain['species']['name'] . "\n";
 
-	$chain = $decoded_result['chain'];
-
-	echo "Evolution Chain:\n";
-
-	while (!empty($chain)) {
-		echo $chain['species']['name'] . "\n";
-		if (isset($chain['evolves_to'])) {
-			if (isset($chain['evolves_to'][0])) {
-				$chain = $chain['evolves_to'][0];
+			//check if there is a variable in the chain with value 'evolves to'
+			if (isset($chain['evolves_to'])) {
+				//Check if there is a value in the array to print (next pokemon in the evolition chain)
+				if (isset($chain['evolves_to'][0])) {
+					$chain = $chain['evolves_to'][0];
+				} else {
+					//stops the loop and indicates the end of the evoliton chain
+					$chain = null;
+				}
+			
 			} else {
 				$chain = null;
 			}
-		
-		} else {
-			$chain = null;
-		}
-	}	
-}
-if ($choice == 'type') {
-	
-	$user_input = readline('Enter a Pokemon type: ');
-	$result = $api->pokemonType($user_input);
-	$decoded_result = json_decode($result, true);
+		}	
+	}
 
-	echo "Type name: " . $decoded_result['name'] . "\n";
-	echo "Damage relations: \n";
-	foreach ($decoded_result['damage_relations'] as $key => $value) {
-    		echo $key . ": \n";
-    	foreach ($value as $name) {
-        	echo $name['name'] . "\n";
-    		}
+	//Checks for when the user wants to know information about a specific type
+	if ($choice == 'type') {
+		
+		$user_input = readline('Enter a Pokemon type: ');
+		$result = $api->pokemonType($user_input);
+		$decoded_result = json_decode($result, true);
+
+		echo "Type name: " . $decoded_result['name'] . "\n";
+		echo "Damage relations: \n";
+
+		//go through each key value pair for damage relations and echo the relation
+		foreach ($decoded_result['damage_relations'] as $key => $value) {
+			echo $key . ": \n";
+		//go through each of the relational damages and echo the name of the types	
+	    	foreach ($value as $name) {
+			echo $name['name'] . "\n";
+	    		}
+		}
+	}
+
+	//Check for when user wants to check specific pokemon's typing
+	if ($choice == 'pokemon type') {
+		$user_input = readline('Enter a Pokemon name: ');
+		$result = $api->pokemon($user_input);
+		$decoded_result = json_decode($result, true);
+		
+		//ouptut the pokemon's name 
+		echo "Pokemon name: " . $decoded_result['name'] . "\n";
+		echo "Types: \n";
+
+		//loop through each type in the array and print to the screen
+		foreach ($decoded_result['types'] as $type) {
+			echo $type['type']['name'] . "\n";
+		}
 	}
 }
-//print_r($chain);
-//echo $decoded_result['name'] . "\n";
 
 ?>
